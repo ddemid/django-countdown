20fbf1af0fb7af13817403740d822f96b29af35a
Iurii Kriachko <iurii.kriachko@gmail.com>
2012-02-07 22:24:59 +0200

    fix import

--- countdown/middleware.py
+++ countdown/middleware.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from django.conf import settings
 from django.views.generic.simple import direct_to_template
+from django.core.urlresolvers import reverse
+
 
 
 class CountdownMiddleware(object):
